e642ddc31fd6fbdced0eb23a60d2a09be23bb78e
Matheus Najjar <matheusnajjar@gmail.com>
2024-07-18 23:06:23 -0700

    feat: creates array of tasks and imports Todo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-// import { useState } from 'react'
-
+import { useState } from 'react'
+import './App.css';
+import Todo from './components/Todo';
 
 function App() {
-
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: "Add feature to system",
+      category: "Work",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Go to gym",
+      category: "Personal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "React improvement",
+      category: "Study",
+      isCompleted: false,
+    }
+  ])
   return (
-    <div>
-      Hello React!
+    <div className='app'>
+      <h1>To Do List</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo todo={todo}/>
+        ))}
+      </div>
     </div>
   )
 }
